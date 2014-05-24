39aad9b5cf52dba941954073d35282730b021957
kmarsafawi <kmarsafawi@gmail.com>
2014-05-24 22:04:46 +0300

    second commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and their may be some benefit
+## to caching the inverse of a matrix rather than compute it repeatedly.
+
 
 ## populates the setter and getter fot the matrix passed
 
